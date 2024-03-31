31f24ecca82333cccd6523e97ba2717150b5fc56
Thomas Poncet <thomas.poncet93@gmail.com>
2024-03-31 20:03:48 +0200

    Add qualification form

---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as QualificationForm } from "./QualificationForm";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from "react"
 import type { HeadFC } from "gatsby"
 import { useForm } from "react-hook-form"
+import Container from 'react-bootstrap/Container';
+import { QualificationForm } from '../components';
 import { searchDPE } from "../lib/";
 
 
@@ -17,14 +20,19 @@ export default function DetailedForm() {
   } = useForm<Inputs>()
 
   return (
-    <form onSubmit={handleSubmit((data) => searchDPE(data.address))}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="1 Allee Sainte Therese 38700 Corenc" {...register("address", { required: true })} />
-      {errors.address && <span>This field is required</span>}
-
-      <input type="submit" />
-    </form>
+    <Container>
+      <h1>Conformètre</h1>
+      <h2>Testez votre confort en un éclair !</h2>
+      <QualificationForm/>
+      <form onSubmit={handleSubmit((data) => searchDPE(data.address))}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="1 Allee Sainte Therese 38700 Corenc" {...register("address", { required: true })} />
+        {errors.address && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
+    </Container>
   )
 }
 
 export const Head: HeadFC = () => <title>Conformètre</title>
+
