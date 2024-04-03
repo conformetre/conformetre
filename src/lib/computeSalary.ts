1e549c6367283b24e7d9945e4109e720a51f17e8
Thomas Poncet <thomas.poncet93@gmail.com>
2024-04-03 21:39:12 +0200

    extract conso by dpe

--- src/lib/computeSalary.ts
+++ src/lib/computeSalary.ts
@@ -1,17 +1,10 @@
-import { EstimatedResults } from "../lib/";
+import { consommations_m2_by_dpe } from "./dpe";
+import type { EstimatedResults } from "../lib/";
+
 
 const elecPrice: number = 0.25
-
-const consommations_m2: { [key: string]: number } = {};
-consommations_m2['A'] = 50
-consommations_m2['B'] = 75
-consommations_m2['C'] = 150
-consommations_m2['D'] = 200
-consommations_m2['E'] = 250
-consommations_m2['F'] = 350
-consommations_m2['G'] = 421
-
 const threshold_tee: number = 0.08
+
 
 export type EconmicResults = {
     minRevenue: number;
@@ -19,10 +12,9 @@ export type EconmicResults = {
 }
 
 export function computeSalary(result: EstimatedResults): EconmicResults {
-    const yearlyCost: number = consommations_m2[result.dpe] * result.surface * elecPrice
+    const yearlyCost: number = consommations_m2_by_dpe[result.dpe] * result.surface * elecPrice
     return {
         minRevenue: result.surface / threshold_tee,
         yearlyCost: yearlyCost
     }
 }
-
---
+++ src/lib/dpe.ts
@@ -0,0 +1,8 @@
+export const consommations_m2_by_dpe: { [key: string]: number } = {};
+consommations_m2_by_dpe['A'] = 50
+consommations_m2_by_dpe['B'] = 75
+consommations_m2_by_dpe['C'] = 150
+consommations_m2_by_dpe['D'] = 200
+consommations_m2_by_dpe['E'] = 250
+consommations_m2_by_dpe['F'] = 350
+consommations_m2_by_dpe['G'] = 421
