a1a6eb19335ec9d85f0b5244d16e2ff7c869b4ca
Thomas Poncet <thomas.poncet93@gmail.com>
2024-04-03 21:41:02 +0200

    add semicolons

--- src/lib/computeSalary.ts
+++ src/lib/computeSalary.ts
@@ -2,19 +2,19 @@ import { consommations_m2_by_dpe } from "./dpe";
 import type { EstimatedResults } from "../lib/";
 
 
-const elecPrice: number = 0.25
-const threshold_tee: number = 0.08
+const elecPrice: number = 0.25;
+const threshold_tee: number = 0.08;
 
 
 export type EconmicResults = {
     minRevenue: number;
     yearlyCost: number;
-}
+};
 
 export function computeSalary(result: EstimatedResults): EconmicResults {
-    const yearlyCost: number = consommations_m2_by_dpe[result.dpe] * result.surface * elecPrice
+    const yearlyCost: number = consommations_m2_by_dpe[result.dpe] * result.surface * elecPrice;
     return {
         minRevenue: result.surface / threshold_tee,
         yearlyCost: yearlyCost
-    }
+    };
 }
