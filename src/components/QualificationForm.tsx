9b8411e4e68dbd0719831d5af1f972d3a5e08fa2
Thomas Poncet <thomas.poncet93@gmail.com>
2024-04-04 23:02:19 +0200

    simplify closed question option type
    use string that we'll parse as number

--- src/components/QualificationForm.tsx
+++ src/components/QualificationForm.tsx
@@ -5,13 +5,11 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ClosedQuestion from './ClosedQuestion';
 
-type Answer = "veryBad" | "bad" | "ok";
-
 type Qualification = {
-  temperature: Answer;
-  humidity: Answer;
-  wind: Answer;
-  resources: Answer;
+  temperature: string;
+  humidity: string;
+  wind: string;
+  resources: string;
 }
 
 type ClosedQuestionProps = {
@@ -19,7 +17,7 @@ type ClosedQuestionProps = {
   question: string;
   options: {
     label: string;
-    value: Answer;
+    value: string;
   }[]
 };
 
@@ -28,36 +26,36 @@ const questions: ClosedQuestionProps[] = [
     name: "temperature",
     question: "En hiver dans mon logement j'ai :",
     options: [
-      { value: "veryBad", label: "Très froid" },
-      { value: "bad", label: "Froid" },
-      { value: "ok", label: "Une bonne température" }
+      { value: "0", label: "Très froid" },
+      { value: "3", label: "Froid" },
+      { value: "6", label: "Une bonne température" }
     ]
   },
   {
     name: "humidity",
     question: "Dans mes pièces de vie (cuisine, salon, chambre) j'ai :",
     options: [
-      { value: "veryBad", label: "De l'humidité et des moisissures" },
-      { value: "bad", label: "De l'humidité" },
-      { value: "ok", label: "Une atmosphère saine" }
+      { value: "0", label: "De l'humidité et des moisissures" },
+      { value: "3", label: "De l'humidité" },
+      { value: "6", label: "Une atmosphère saine" }
     ]
   },
   {
     name: "wind",
     question: "A l'intérieur je ressens des courants d'air :",
     options: [
-      { value: "veryBad", label: "Souvent" },
-      { value: "bad", label: "Parfois" },
-      { value: "ok", label: "Jamais" }
+      { value: "0", label: "Souvent" },
+      { value: "2", label: "Parfois" },
+      { value: "4", label: "Jamais" }
     ]
   },
   {
     name: "resources",
     question: "Il m'arrive de me priver (alimentation, loisirs) pour payer ma facture d'énergie :",
     options: [
-      { value: "veryBad", label: "Souvent" },
-      { value: "bad", label: "Parfois" },
-      { value: "ok", label: "Jamais" }
+      { value: "0", label: "Souvent" },
+      { value: "2", label: "Parfois" },
+      { value: "4", label: "Jamais" }
     ]
   }
 ];
@@ -77,7 +75,7 @@ export default function QualificationForm() {
           <ClosedQuestion
             key={name}
             question={question}
-            register={register(name)}
+            register={register(name, { required: true })}
             options={options}
           />
         )}
